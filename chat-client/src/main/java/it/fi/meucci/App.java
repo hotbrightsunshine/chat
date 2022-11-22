5fc63583b9aab00a725409ae8b2484a952f18150
lai2-01 <lai2-01@itismeucci.com>
2022-11-22 08:58:28 +0100

    before merging chat-client and chat-client-temp

--- chat-client/src/main/java/it/fi/meucci/App.java
+++ chat-client/src/main/java/it/fi/meucci/App.java
@@ -6,8 +6,6 @@ import java.net.Inet4Address;
 public class App
 {
 
-    public static final int PORT = 7777;
-    public static final Inet4Address ADDRESS = (Inet4Address) Inet4Address.getLoopbackAddress();
 
     public static void main( String[] args ) throws IOException {
         Client c = new Client(ADDRESS, PORT);
