bd9bd71cf51349461abe33e86cbde6d70f550e0e
lai2-01 <lai2-01@itismeucci.com>
2022-11-29 10:02:25 +0100

    first scene

--- chat-client/src/main/java/it/fi/meucci/App.java
+++ chat-client/src/main/java/it/fi/meucci/App.java
@@ -7,6 +7,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.Inet4Address;
 
 /**
  * JavaFX App
@@ -34,8 +35,8 @@ public class App extends Application {
         return fxmlLoader.load();
     }
 
-    public static void main(String[] args) {
-        Client c = new Client(ADDRESS, PORT);
+    public static void main(String[] args) throws IOException {
+       //Client c = new Client(ADDRESS, PORT);
         launch();
     }
 
---
+++ opensb.sh
@@ -0,0 +1,2 @@
+cd ./chat-client/src/main/resources/it/fi/meucci
+/opt/scenebuilder/bin/SceneBuilder ./primary.fxml 
