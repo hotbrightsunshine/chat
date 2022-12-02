8e72485117ccf69cb8798cdb89c36f83602afad4
Lorenzo Salanitro <78509205+LorenzoSalanitro@users.noreply.github.com>
2022-12-02 18:30:33 +0100

    gui update

--- chat-client/src/main/java/it/fi/meucci/SecondaryController.java
+++ chat-client/src/main/java/it/fi/meucci/SecondaryController.java
@@ -6,6 +6,13 @@ import javafx.fxml.FXML;
 public class SecondaryController {
 
     @FXML
+    @FXML Button leave;
+    @FXML Button sendMess;
+    @FXML ListView listaPart;
+    @FXML ListView chat;
+    @FXML TextField ip;
+    @FXML TextField doveScrivi;
+
     private void switchToPrimary() throws IOException {
         App.setRoot("primary");
     }
