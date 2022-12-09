f939e4f275e7c0c2d7d6d440de89b365631dfbca
Francesco <7frapar@gmail.com>
2022-12-09 17:15:10 +0100

    specified controllers

--- chat-client/src/main/java/it/fi/meucci/App.java
+++ chat-client/src/main/java/it/fi/meucci/App.java
@@ -43,8 +43,9 @@ public class App extends Application {
             System.out.println("Connessione rifiutata. ");
         }
 
-        //launch();
+        launch();
         System.out.println("CIAO");
+        return;
     }
 
 }
