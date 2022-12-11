7c4c98053d1b13810140515da01597ac803a210b
Lorenzo Salanitro <78509205+LorenzoSalanitro@users.noreply.github.com>
2022-12-11 19:22:14 +0100

    Handlemethods added

--- terminal-chat/src/main/java/it/meucci/HandleMethods.java
+++ terminal-chat/src/main/java/it/meucci/HandleMethods.java
@@ -1,11 +1,62 @@
 package it.meucci;
 
 public abstract class HandleMethods {
-    static boolean left(String username){
+
+    private static String stampa;
+
+    static boolean left(String username)
+    {
         return App.client.getUsernames().remove(username);
     }
 
-    static boolean join(String username){
+    static boolean join(String username)
+    {
         return App.client.getUsernames().add(username);
+    }
+
+    static void name(String username)
+    {
+        if (App.client.getUsernames().equals(username))
+        {
+            System.out.println("username gia presente");
+        }
+        else
+        {
+            App.client.getUsernames().add(username);
+        }
+    }
+
+    static void needName(String username)
+    {
+        if (App.client.getUsernames().equals(null)) 
+        {
+            System.out.println("inserire username");
+        }
+    }
+
+    static String list()
+    {
+        for (int i = 0; i < App.client.getUsernames().size(); i++)
+        {
+            stampa = App.client.getUsernames().get(i);
+        }
+        
+        return stampa;
+    }
+
+    static void desNotOk()
+    {
+
+    }
+
+    static void disconnect()
+    {
+        App.client.getUsernames();
+    }
+
+    static void usernameChanged(String newUsername)
+    {
+        
+        
     }
 }
