59709b753a2af6d6fc7a46cc4977194a79da66f5
Francesco <7frapar@gmail.com>
2022-12-13 21:51:14 +0100

    deleted stuff

--- terminal-chat/src/main/java/it/meucci/textinterface/pages/Page.java
+++
@@ -1,9 +0,0 @@
-package it.meucci.textinterface.pages;
-
-import java.util.ArrayList;
-
-public interface Page {
-    boolean isTextInputEnabled();
-    String getHeader();
-    ArrayList<String> getContent();
-}
