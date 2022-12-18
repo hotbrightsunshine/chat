8a412bf213222632bd3fc6550f1968f8c7dfe2e7
Francesco <7frapar@gmail.com>
2022-12-18 14:57:02 +0100

    adding javadoc

---
+++ docs/javadoc/client/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"it.meucci"},{"l":"it.meucci.commands"},{"l":"it.meucci.utils"}];updateSearchResults();
---
+++ docs/javadoc/server/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"it.fi.meucci"},{"l":"it.fi.meucci.exceptions"},{"l":"it.fi.meucci.logger"},{"l":"it.fi.meucci.utils"}];updateSearchResults();
